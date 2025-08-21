352149dc4e85bb3b2523c4a6ddb418771c1fb396
Shivansh <151555468+Quantumdevlpr@users.noreply.github.com>
2025-08-21 18:15:47 +0530

    "final "

--- Problems/Day4.java
+++ Problems/Day4.java
@@ -1,35 +1,22 @@
 import java.util.*;
-
 public class Day4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();   // number of friends
-        int k = sc.nextInt();   // number of bottles
-        int l = sc.nextInt();   // ml per bottle
-        int c = sc.nextInt();   // number of limes
-        int d = sc.nextInt();   // slices per lime
-        int p = sc.nextInt();   // grams of salt
-        int nl = sc.nextInt();  // ml needed per toast per person
-        int np = sc.nextInt();  // grams salt needed per toast per person
-        
-        // Total drink (ml)
-        int totalDrink = k * l;
-        int possibleFromDrink = totalDrink / nl;
-        
-        // Total lime slices
-        int totalSlices = c * d;
-        
-        // Total possible from salt
-        int possibleFromSalt = p / np;
-        
-        // The limiting factor
-        int totalToasts = Math.min(possibleFromDrink, 
-                            Math.min(totalSlices, possibleFromSalt));
-        
-        // Each friend gets an equal number of toasts
-        System.out.println(totalToasts / n);
-        
+        //n, k, l, c, d, p, nl, np
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int l = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int p = sc.nextInt();
+        int nl = sc.nextInt();
+        int np = sc.nextInt();
+        int amount = k*l;
+        int quantity = amount   /n;
+        int limes = c*d;
+        int salt = p/np;
+        int result = Math.min(Math.min(quantity,limes),salt);
+        System.out.println(result/n);
         sc.close();
     }
 }
